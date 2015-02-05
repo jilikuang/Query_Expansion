4b77ab3de4b0588d2e7ac27d44581f7b78d1f7ab
Jie-Gang Kuang <jk3735@columbia.edu>
2015-02-05 12:58:32 -0500

    Loop the search routine

--- main.py
+++ main.py
@@ -17,56 +17,78 @@ def main(argv):
         return
     if len(argv) > 2 and argv[0].startswith('--key='):
         bing_key = argv[0][6:]
-        precision = float(argv[1])
+        target = float(argv[1])
         query = argv[2:]
     else:
         # Use the default Bing key
         bing_key = '6SBqV1l4XaDnfx+w4rPVqyEgtNJqvUJu50wXdg5njhk'
-        precision = float(argv[0])
+        target = float(argv[0])
         query = argv[1:]
 
-    # Start of searching process (to loop)
-    print 'Parameters:'
-    print 'Client key = ' + bing_key
-    print 'Query      = ' + search_engine.format_query(query)
-    print 'Precision  = ' + str(precision)
-
+    # Initalization for routine
     se = search_engine.get_engine(bing_key)
-    result = se.search(query)
+    routine_cnt = 0
+    precision = 0
 
-    if True:
-        xml_file = save_xml(result, 0)
-        print 'Resuls is saved as ' + xml_file
-        result = open(xml_file, 'r')
+    # Start of searching process (to loop)
+    while precision < target:
+        routine_cnt += 1
+        print 'Parameters:'
+        print 'Client key = ' + bing_key
+        print 'Query      = ' + search_engine.format_query(query)
+        print 'Precision  = ' + str(target)
 
-    entries = xml_parser.parse_entries(result)
+        result = se.search(query)
 
-    print 'Total no of results: ' + str(len(entries))
-    if len(entries) < 10:
-        print 'Abort due to fewer than 10 results'
-        return
+        if True:
+            xml_file = save_xml(result, routine_cnt)
+            print 'Resuls is saved as ' + xml_file
+            result = open(xml_file, 'r')
 
-    # User judgement and generate list for algo
-    docs = []
+        entries = xml_parser.parse_entries(result)
 
-    print 'Bing search results:'
-    print '======================'
+        print 'Total no of results: ' + str(len(entries))
+        if len(entries) < 10:
+            print 'Abort due to fewer than 10 results'
+            break
 
-    for i in range(len(entries)):
-        url = entries[i].get_url()
-        title = entries[i].get_title()
-        desc = entries[i].get_description()
-        print 'Result ' + str(i+1)
-        print '['
-        print ' URL: ' + url
-        print ' Title: ' + title
-        print ' Summary: ' + desc
-        print ']'
-        relevance = True
-        judge = raw_input('Relevant (Y/N)? ')
-        if judge == 'N' or judge == 'n':
-            relevance = False
-        docs.append(Document.Document(title, url, desc, relevance))
+        # User judgement and generate list for algo
+        docs = []
+        accept_cnt = 0.0
+        print 'Bing Search Results:'
+        print '======================'
+
+        for i in range(len(entries)):
+            url = entries[i].get_url()
+            title = entries[i].get_title()
+            desc = entries[i].get_description()
+            print 'Result ' + str(i+1)
+            print '['
+            print ' URL: ' + url
+            print ' Title: ' + title
+            print ' Summary: ' + desc
+            print ']'
+            relevance = True
+            judge = raw_input('Relevant (Y/N)? ')
+            if judge == 'N' or judge == 'n':
+                relevance = False
+            else:
+                accept_cnt += 1
+            docs.append(Document.Document(title, url, desc, relevance))
+
+        # Check precision
+        print str(accept_cnt) + ' ' + str(len(entries))
+        precision = accept_cnt / len(entries)
+        print '======================'
+        print 'Feedback Summary'
+        print 'Query ' + search_engine.format_query(query)
+        print 'Precision ' + str(precision)
+        if precision >= target:
+            print 'Desired precision reached, done'
+            break
+
+        # Apply algo
+        print 'Still below the desired precision of ' + str(target)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
