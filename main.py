f89f9fd2ce9ab1b43f7e41f82a2572579eda6880
Jie-Gang Kuang <jk3735@columbia.edu>
2015-02-04 19:23:57 -0500

    Modify the input format for main

--- main.py
+++ main.py
@@ -7,18 +7,21 @@ def main(argv):
     if len(argv) < 2:
         print 'Too few arguments'
         return
-    if len(argv) < 3:
-        bing_key = 'default Bing Account Key'
-        precision = float(argv[0])
-        query = argv[1:]
-    else:
-        bing_key = argv[0]
+    if len(argv) > 2 and argv[0].startswith('--key='):
+        bing_key = argv[0][6:]
         precision = float(argv[1])
         query = argv[2:]
+    else:
+        # Use the default Bing key
+        bing_key = '6SBqV1l4XaDnfx+w4rPVqyEgtNJqvUJu50wXdg5njhk'
+        precision = float(argv[0])
+        query = argv[1:]
 
-    print 'Bing Account Key: ' + bing_key
-    print 'Precision: ' + str(precision)
-    print 'Query: ' + str(query)
+    # Start of searching process (to loop)
+    print 'Parameters:'
+    print 'Client key = ' + bing_key
+    print 'Query      = ' + str(query)
+    print 'Precision  = ' + str(precision)
 
     #query = 'Query=%27gates%27&$top=10&$format=Atom'
     #res = SearchEngine.SearchEngine.getResult(query)
