c836bba1432376ac142cf7743aab2b518ee6322f
di-ruan <ruandi88@gmail.com>
2015-02-05 11:01:46 -0500

    add isrelevant

--- Document.py
+++ Document.py
@@ -1,6 +1,7 @@
 class Document:
 
-    def __init__(self, title, url, des):
+    def __init__(self, title, url, des, relevant):
         self.title = title
         self.url = url
         self.description = des
+        self.is_relevant = relevant
